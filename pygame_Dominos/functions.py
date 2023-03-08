60e93833a35c7e6c5fbc793799aa99c7eae61f90
Alec Autry <alecautry20181@gmail.com>
2023-03-08 08:24:34 -0600

    updated code
    Trickwinner might be finished. Ready for testing

--- pygame_Dominos/functions.py
+++ pygame_Dominos/functions.py
@@ -42,26 +42,70 @@ def trickWinner(dlead, d2, d3, d4):
         return d4
     
     # No double, no trumps, have to compare more dlead high
+    # If dlead is highest it will always win
     if dlead > d2 and (dlead > d3) and (dlead > d4):
         return dlead
     # if dlead not the highest, have to compare dlead high to both sides
-    elif dlead_high == (d2 & high) or (dlead_high == (d2 * low)): # If dlead high is the same 
-        return 2
+    elif dlead_high == (d2 & high) or (dlead_high == (d2 & low)): # If d2 matches dlead high
+        if (dlead_high == (d3 & high)) or (dlead_high == (d3 & low)): # If d3 matches dlead high
+            if (dlead_high == (d4 & high)) or (dlead_high == (d4 & low)): # If d4 matches dlead high
+                # If all dominos share a lead rerturn the highest
+                if dlead > d2 and (dlead > d3) and (dlead >d4):
+                    return dlead
+ 
+                elif d2 > d3 and (d2 > d4): # if not dlead, check d2
+                    return d2
+ 
+                elif d3 > d4: # if not d2, check d3
+                    return d3
+ 
+                else:   # has to be d4
+                    return d4
+ 
+            # If D4 doesn't match but dlead, d2 and d3 match
+            elif dlead > d2 and (dlead > d3):
+                return dlead
+ 
+            elif d2 > d3:
+                return d2
+ 
+            else:
+                return d3
+        #If only d2 and dlead match
+        elif dlead > d2:
+            return dlead
+        
+        else:
+            return d2
+ 
+    #if d2 doesn't match
+    elif (dlead_high == (d3 & high) and (dlead_high == (d3 & low)): # If dlead mathces d3
+        if dlead_high == (d4 & high) and (dlead_high == (d4 & low)): # if dlead matched d4
+            if dlead > d3 and (dlead > d4): # compare higher
+                return dlead
 
+            elif d3 > d4: # if not dlead then d3
+                return d3
+ 
+            else: # has to be d4
+                return d4
+ 
+        # if dlead only matches d3
+        elif dlead > d3:
+            return dlead
+ 
+        else:
+            return d3
     
+    # if only d4 matches          
+    elif dlead_high == (d4 & high) and (dlead_high == (d4 & low)):
+        if dlead > d4:
+            return dlead
+
+        else:
+            return d4
+
+    # If nothing matches dlead then it walks (Wins) 
+    else:
+        return dlead 
     
-
-# RULES OF 42 with Bit operations
-
-# If double Trump
-# If Trump and nothing else
-# If highest trump
-
-# If trump pick highest value domino 
-
-# If double of leadSuit
-# if dlead_high & dlead
-
-
-# If highest of suit
-# if only of suit
