979ac8bf38ecfb2e463d7ba7785bec94915ad7da
alecautry <alecautry@protonmail.com>
2025-01-20 16:16:03 -0600

    adding server and client code. Examples for now.

---
+++ client.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+
+class GameClient:
+    def __init__(self, host='localhost', port=12345):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((host, port))
+
+    def send_data(self, data):
+        self.client.send(data.encode('utf-8'))
+
+    def receive_data(self):
+        while True:
+            try:
+                data = self.client.recv(1024).decode('utf-8')
+                if not data:
+                    break
+                print(f"Received: {data}")
+            except:
+                break
+
+    def start(self):
+        receive_thread = threading.Thread(target=self.receive_data)
+        receive_thread.start()
+
+if __name__ == "__main__":
+    client = GameClient()
+    client.start()
+    while True:
+        message = input("Enter message: ")
+        client.send_data(message)
---
+++ server.py
@@ -0,0 +1,47 @@
+import socket
+import threading
+
+class GameServer:
+    def __init__(self, host='localhost', port=12345):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((host, port))
+        self.server.listen(4)  # Listen for up to 4 connections
+        self.clients = []
+        self.lock = threading.Lock()
+
+    def handle_client(self, client_socket):
+        while True:
+            try:
+                data = client_socket.recv(1024).decode('utf-8')
+                if not data:
+                    break
+                self.broadcast(data, client_socket)
+            except:
+                break
+        client_socket.close()
+        with self.lock:
+            self.clients.remove(client_socket)
+
+    def broadcast(self, message, client_socket):
+        with self.lock:
+            for client in self.clients:
+                if client != client_socket:
+                    try:
+                        client.send(message.encode('utf-8'))
+                    except:
+                        client.close()
+                        self.clients.remove(client)
+
+    def start(self):
+        print("Server started...")
+        while True:
+            client_socket, addr = self.server.accept()
+            print(f"Connection from {addr}")
+            with self.lock:
+                self.clients.append(client_socket)
+            client_handler = threading.Thread(target=self.handle_client, args=(client_socket,))
+            client_handler.start()
+
+if __name__ == "__main__":
+    server = GameServer()
+    server.start()
