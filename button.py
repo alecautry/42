d42286e7d9e533016073e4b297acc408d8e332c3
alecautry <alecautry@protonmail.com>
2025-06-08 20:48:42 -0500

    major commit

--- button.py
+++ button.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Button:
     def __init__(self, x, y, width, height, text, font, bg_color, text_color, highlight_color=None, image=None):
         self.rect = pygame.Rect(x, y, width, height)
